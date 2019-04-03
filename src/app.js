0a5d6e734ec950a2169dee34dad4fb6e0b5a5a0e
Daniele Bolla <bolladaniele@gmail.com>
2019-04-03 13:04:34 +0100

    adds styles

--- src/app.js
+++ src/app.js
@@ -1,11 +1,9 @@
-import './style.css'
 import 'css-reset-and-normalize'
+import './style.css'
+
 
 /**Dom */
 const selectEl = (selector, parent = document) => parent.querySelector(selector)
-const selectEls = (selector, parent = document) => parent.querySelectorAll(selector)
-const show = elem => elem.style.display = 'block'
-const hide = elem => elem.style.display = 'none'
 const createNodeFromTemplate = (template, wrapper, wrapperAttrs) => {
 	const node = document.createElement(wrapper)
 	for (const [key, value] of Object.entries(wrapperAttrs)) {
@@ -22,11 +20,10 @@ const sliceBy = (list, start, end) => list.slice(start, end)
 /**Templates */
 const useArticleTemplate = (url, title) => (`
 	<h2><a target="_blank" onclick="event.stopPropagation()" href="${url}">${title}</a></h2>
-	<button>Load Comment</button>
+	<button class="btn-comments-loader">load comments</button>
 `)
 const useCommentsTemplate = (text, by) => (`
-	<h3>${by}</h3>
-	<hr/>
+	<h3>&raquo; by: <strong>${by}</strong></h3>
 	<p>${text}</p>
 `)
 
@@ -52,33 +49,31 @@ const main = () => {
 	})
 
 	/**Streamers and Consumers*/
-	async function* articlesStreamer(ids, start = 0) {
-		let obj = { articles: [], start: start }
-		const urls = sliceBy(ids, obj.start, obj.start + 10).map(id => `${baseApi}/item/${id}.json?print=pretty`)
-		obj.articles = await Promise.all((await urls.map(fetchAsyncA)))
-		obj.start = obj.start + obj.articles.length
-		yield obj;
+	async function* articlesStreamer(ids, slice, start = 0, end) {
+		const urls = sliceBy(ids, start, start + slice).map(id => `${baseApi}/item/${id}.json?print=pretty`)
+		yield await Promise.all((await urls.map(fetchAsyncA)))
 	}
 	const articlesConsumer = async () => {
-		const articlesStream = articlesStreamer(context.topStories, context.articles.length)
-		const { value: { articles } } = await articlesStream.next()
+		const articlesStream = articlesStreamer(context.topStories, 10, context.articles.length)
+		const { value:articles } = await articlesStream.next()
 		context.articles = [...context.articles, ...articles]
 		articles.forEach(({ url, title, id, kids}) => {
 			const template = useArticleTemplate(url, title)
 			const attrs = { 'class': `article article-${id}`, kids, id }
 			const node = createNodeFromTemplate(template, 'article', attrs)
-			$wrap.insertAdjacentElement('beforeend', node)
+			$wrap.appendChild(node)
 		})
 		$wrap.appendChild($sentinel)
 	}
 	const commentsConsumer = async (ids, length, currentComponent) => {
-		const commentsStream = articlesStreamer(ids, length)
-		const { value: { articles } } = await commentsStream.next()
-		articles.forEach(({id, text, by}) => {
+		const commentsStream = articlesStreamer(ids, 3, length)
+		const { value:comments } = await commentsStream.next()
+		comments.forEach(({id, text, by}) => {
 			const template = useCommentsTemplate(text, by)
 			const attrs = { 'class': `comment comment-${id}`, id }
 			const node = createNodeFromTemplate(template, 'div', attrs)
-			currentComponent.insertAdjacentElement('beforeend', node)
+			const button = selectEl('.btn-comments-loader', currentComponent)
+			currentComponent.insertBefore(node, button)
 		})
 	}
 
